1f3d911c612bdf3332be523c72cd2e10c63020e7
Daniel Wayota <danielglez1990@gmail.com>
2020-03-01 18:40:23 +0000

    Scripts and basic player movement

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    public float speed = 4f;
+
+    private Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(h, v, 0);
+
+        this.body.velocity = movement * speed;
+    }
+}
