a8612a1098bcf2c60e7fa260e0d4aa96a5b24a4e
Daniel Wayota <danielglez1990@gmail.com>
2020-03-06 15:09:08 +0000

    Font added and GameOver screen

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,9 +2,24 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainMenuPanel;
+    public GameObject controlsPanel;
+
     public void Play()
     {
         LevelLoader.Load(LevelNames.GAME);
+    }
+
+    public void Controls()
+    {
+        this.mainMenuPanel.SetActive(false);
+        this.controlsPanel.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        this.mainMenuPanel.SetActive(true);
+        this.controlsPanel.SetActive(false);
     }
 
     public void Quit()
